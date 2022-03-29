e8a13145bd1636a1ca7f41f65c983ef995e67f67
joaolfms <83645646+joaolfms@users.noreply.github.com>
2022-03-29 15:46:46 -0300

    Update helpers.py
    Comentando as funções do codigo

--- helpers.py
+++ helpers.py
@@ -1,8 +1,6 @@
 import json
 
 def get_log_file(file_path): 
-# Função para receber e retornar o arquivo em formato de leitura por linhas.
-
     file = open(file_path)
 
     try:
@@ -11,10 +9,9 @@ def get_log_file(file_path):
         file.close()
     
     return(arquivo)
+# Função para receber e retornar o arquivo em formato de leitura por linhas.
 
 def get_player_data(linha):
-# Função que mapeia o arquivo de log e retorna o id e o nome do player.
-
     i = 0
     name_start = 0
     nome_do_player = ""
@@ -35,11 +32,10 @@ def get_player_data(linha):
 
     
     return(nome_do_player, _id)
+# Função que mapeia o arquivo de log e retorna o id e o nome do player.
 
 
 def get_kill_data(linha):
-# função que mapeia o arquivo de log e retona o nome do assassino e o nome do morto.
-
     i = 0
     contador_dois_pontos = 0
     for l in linha:
@@ -60,11 +56,9 @@ def get_kill_data(linha):
         i += 1
     
     return(assassino, morto)
+# função que mapeia o arquivo de log e retona o nome do assassino e o nome do morto.
 
-
-def json_writer(games_list):
-# Função que escreve em json as informações necessárias do game. 
-    
+def json_writer(games_list):    
     with open('data.json', 'w', encoding='utf-8') as f:
         contador = 1
         f.writelines("[")
@@ -85,3 +79,4 @@ def json_writer(games_list):
                 f.writelines("\n},\n")
             contador += 1 
         f.writelines("]")
+# Função que escreve em json as informações necessárias do game. 
