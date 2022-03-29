559649c04d52e98c1e675ae9d7bc5e42ae0da123
Joao Lucas <jhonfmsvencedor@gmail.com>
2022-03-29 14:54:24 -0300

    Comentando as funçoes do codigo

--- script.py
+++ script.py
@@ -1,15 +1,18 @@
 import os
 from classes import Player, Game
 from helpers import get_log_file, get_player_data, get_kill_data, json_writer
-
+# Importações necessárias para execução do programa.
     
 arquivo = get_log_file(os.environ.get('PATH_TO_LOG_FILE') or "Quake.txt")
+# Definindo o arquivo a ser mapeado.
 
-games = []
+games = [] 
+# Lista com os games mapeados no log.
 
 for linha in arquivo:
     if "InitGame" in linha:
         game = Game()
+# Condição para definir o inicio de cada game.
 
     if "ClientUserinfoChanged" in linha:
         player_ja_existe = 0
@@ -24,6 +27,7 @@ for linha in arquivo:
 
         if player_ja_existe != 1: 
             game.players.append(player)
+# Condição para definir e atualizar o nome de cada player.
 
     if "Kill" in linha:
         game.kills += 1
@@ -37,10 +41,13 @@ for linha in arquivo:
             for p in game.players:
                 if p.nome == assassino and p.nome != morto:
                     p.kills += 1 
+# Condição para deinir o número de kills para cada players.
 
 
 
     if "ShutdownGame:" in linha:
         games.append(game)
+# Condição que defini o final de um game e adiciona-o a lista de games com suas informações.
 
 json_writer(games)
+# Retorna o arquivo em json.
